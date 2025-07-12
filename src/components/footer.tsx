c789a95a72aca02e5e654c7f409207e1964e59fc
Nilesh Pahari <nileshpahari@protonmail.com>
2025-07-13 03:01:00 +0530

    add footer and navbar

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export function Footer() {
+    return (
+        <footer className="flex flex-col gap-4 justify-center items-between p-4 py-6">
+            <div className="w-full border border-gray-200"></div>
+            <p>Developed by Nilesh: <Link href="https://nileshkrpahari.xyz" className="underline">nileshkrpahari.xyz</Link></p>
+        </footer>
+    );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggler } from "./ThemeToggler";
+import Logo from "@/components/logo";
+export function Navbar() {
+  return (
+    <div className="flex flex-row justify-between items-center p-4">
+      <Logo/>
+      <div><ThemeToggler/></div>
+    </div>
+  );
+}

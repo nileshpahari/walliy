7bbf0db0e409801c87e64a2636e3b5e49727d39f
Nilesh Pahari <nileshpahari@protonmail.com>
2025-07-14 23:50:05 +0530

    add logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <h1
+      className=" scroll-m-20 text-3xl font-bold font-arcade lg:text-4xl text-[var(--sidebar-primary)] leading-snug"
+      style={{
+        textShadow: 'var(--shadow-sm)',
+      }}
+    >
+      WALLIY
+    </h1>
+  )
+}

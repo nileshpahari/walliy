d519116844c02c9a38d0eb217444485d669c7d95
Nilesh Pahari <nileshpahari@protonmail.com>
2025-07-14 23:52:58 +0530

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export function Footer() {
     return (
-        <footer className="flex flex-col gap-4 justify-center items-between p-4 py-6 mt-4">
+        <footer className="flex flex-col gap-4 justify-center items-between p-4 py-6 mt-4 font-arcade  text-[var(--sidebar-primary)] leading-snug">
             <div className="w-full border border-gray-200"></div>
             <p>Developed by Nilesh: <Link href="https://nileshkrpahari.xyz" className="underline">nileshkrpahari.xyz</Link></p>
         </footer>

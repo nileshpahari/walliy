bfc728c623a697576f83c512b631c1be572d6596
Nilesh Pahari <nileshpahari@protonmail.com>
2025-07-26 21:20:03 +0530

    enable websassembly and temporarily disable linting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;

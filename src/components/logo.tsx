38b587ed8d5b5d090ef9c9f81a26ff0bcddc6685
Nilesh Pahari <nileshpahari@protonmail.com>
2025-07-27 03:32:57 +0530

    update favicon and logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,7 @@
-"use client"
-
-import { Wallet } from "lucide-react"
-
 export default function Logo() {
   return (
-    <div className="flex items-center space-x-2">
-      <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary">
-        <Wallet className="h-4 w-4 text-primary-foreground" />
-      </div>
-      <span className="font-semibold text-2xl">Walliy</span>
-    </div>
-  )
+    <span className="text-2xl md:text-3xl font-semibold tracking-tight dark:text-primary-foreground text-primary font-mono">
+      Walliy
+    </span>
+  );
 }
